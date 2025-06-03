cbed4b03ecc24dbaa2faa40d1c82f405f2a69d61
Davide Arcolini <davidearcolini.dev@gmail.com>
2025-06-03 19:31:30 +0200

    feat: added second round of instructions

--- mgr.js
+++ mgr.js
@@ -1,159 +1,177 @@
 define(['managerAPI',
-		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
+    'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function (Manager) {
 
 
-	//You can use the commented-out code to get parameters from the URL.
-	//const queryString = window.location.search;
-    //const urlParams = new URLSearchParams(queryString);
-    //const pt = urlParams.get('pt');
+        //You can use the commented-out code to get parameters from the URL.
+        //const queryString = window.location.search;
+        //const urlParams = new URLSearchParams(queryString);
+        //const pt = urlParams.get('pt');
 
-	var API    = new Manager();
-	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'Vra6BAwiEA0Z',  {file_type:'csv'});	
+        var API = new Manager();
+        //const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
+        init_data_pipe(API, 'Vra6BAwiEA0Z', { file_type: 'csv' });
 
-    API.setName('mgr');
-    API.addSettings('skip',true);
+        API.setName('mgr');
+        API.addSettings('skip', true);
 
-    //Randomly select which of two sets of category labels to use.
-    let raceSet = API.shuffle(['a','b'])[0];
-    let blackLabels = [];
-    let whiteLabels = [];
+        //Randomly select which of two sets of category labels to use.
+        let raceSet = API.shuffle(['a', 'b'])[0];
+        let blackLabels = [];
+        let whiteLabels = [];
 
-    if (raceSet == 'a') {
-        blackLabels.push('Afroamericani');
-        whiteLabels.push('Europei');
-    } else {
-        blackLabels.push('Persone nere');
-        whiteLabels.push('Persone bianche');
-    }
+        if (raceSet == 'a') {
+            blackLabels.push('Afroamericani');
+            whiteLabels.push('Europei');
+        } else {
+            blackLabels.push('Persone nere');
+            whiteLabels.push('Persone bianche');
+        }
 
-    API.addGlobal({
-        raceiat:{},
-        //YBYB: change when copying back to the correct folder
-        baseURL: './images/',
-        raceSet:raceSet,
-        blackLabels:blackLabels,
-        whiteLabels:whiteLabels,
-        //Select randomly what attribute words to see. 
-        //Based on Axt, Feng, & Bar-Anan (2021).
-        posWords : API.shuffle([
-            'Amore', 'Allegria', 'Amico', 'Piacere',
-            'Adorare', 'Allegro', 'Amicizia', 'Gioioso', 
-            'Sorridente','Apprezzare', 'Eccellente', 'Felice', 
-            'Gioioso', 'Spettacolare', 'Attraente', 'Delizia', 
-            'Eccitazione', 'Ridere', 'Attraente','Delizioso', 
-            'Favoloso', 'Glorioso', 'Piacevole', 'Bello', 
-            'Fantastico', 'Felice', 'Amorevole', 'Eccezionale', 
-            'Celebrare', 'Divertirsi', 'Magnifico', 'Trionfo'
-        ]), 
-        negWords : API.shuffle([
-            'Abuso', 'Lutto', 'Veleno', 'Tristezza', 
-            'Dolore', 'Disprezzare', 'Fallimento', 'Sgradevole', 
-            'Arrabbiato', 'Detestare', 'Orribile', 'Negativo', 
-            'Brutto', 'Sporco', 'Schifoso', 'Malvagio', 
-            'Marcio','Infastidire', 'Disastro', 'Terrificante',  
-            'Disprezzo', 'Terribile', 'Disgusto', 'Odiare', 
-            'Umiliare', 'Egoista', 'Tragico', 'Fastidioso', 
-            'Odio', 'Doloroso', 'Nauseante', 'Rivoltante'
-        ])
-    });
+        API.addGlobal({
+            raceiat: {},
+            //YBYB: change when copying back to the correct folder
+            baseURL: './images/',
+            raceSet: raceSet,
+            blackLabels: blackLabels,
+            whiteLabels: whiteLabels,
+            //Select randomly what attribute words to see. 
+            //Based on Axt, Feng, & Bar-Anan (2021).
+            posWords: API.shuffle([
+                'Amore', 'Allegria', 'Amico', 'Piacere',
+                'Adorare', 'Allegro', 'Amicizia', 'Gioioso',
+                'Sorridente', 'Apprezzare', 'Eccellente', 'Felice',
+                'Gioioso', 'Spettacolare', 'Attraente', 'Delizia',
+                'Eccitazione', 'Ridere', 'Attraente', 'Delizioso',
+                'Favoloso', 'Glorioso', 'Piacevole', 'Bello',
+                'Fantastico', 'Felice', 'Amorevole', 'Eccezionale',
+                'Celebrare', 'Divertirsi', 'Magnifico', 'Trionfo'
+            ]),
+            negWords: API.shuffle([
+                'Abuso', 'Lutto', 'Veleno', 'Tristezza',
+                'Dolore', 'Disprezzare', 'Fallimento', 'Sgradevole',
+                'Arrabbiato', 'Detestare', 'Orribile', 'Negativo',
+                'Brutto', 'Sporco', 'Schifoso', 'Malvagio',
+                'Marcio', 'Infastidire', 'Disastro', 'Terrificante',
+                'Disprezzo', 'Terribile', 'Disgusto', 'Odiare',
+                'Umiliare', 'Egoista', 'Tragico', 'Fastidioso',
+                'Odio', 'Doloroso', 'Nauseante', 'Rivoltante'
+            ])
+        });
 
-    API.addTasksSet({
-        instructions: [{
-            type: 'message',
-            buttonText: 'Continue'
-        }],
+        API.addTasksSet({
+            instructions: [{
+                type: 'message',
+                buttonText: 'Continue'
+            }],
 
-        intro: [{
-            inherit: 'instructions',
-            name: 'intro',
-            templateUrl: 'intro.jst',
-            title: 'Intro',
-            header: 'Welcome'
-        }],
+            intro: [{
+                inherit: 'instructions',
+                name: 'intro',
+                templateUrl: 'intro.jst',
+                title: 'Intro',
+                header: 'Welcome'
+            }],
 
-        raceiat_instructions: [{
-            inherit: 'instructions',
-            name: 'raceiat_instructions',
-            templateUrl: 'raceiat_instructions.jst',
-            title: 'IAT Instructions',
-            header: 'Implicit Association Test'
-        }],
+            // Second intro (for second round)
+            intro_2: [{
+                inherit: 'instructions',
+                name: 'intro_2',
+                templateUrl: 'intro_2.jst',  // Use a different .jst if needed
+                title: 'Intro - Round 2',
+                header: 'Second Round'
+            }],
 
-        explicits: [{
-            type: 'quest',
-           name: 'explicits',
-            scriptUrl: 'explicits.js'
-       }],
+            raceiat_instructions: [{
+                inherit: 'instructions',
+                name: 'raceiat_instructions',
+                templateUrl: 'raceiat_instructions.jst',
+                title: 'IAT Instructions',
+                header: 'Implicit Association Test'
+            }],
 
-        raceiat: [{
-            type: 'time',
-            name: 'raceiat',
-            scriptUrl: 'raceiat.js'
-        }],
+            //This task is used to show the explicits questions.
+            raceiat_instructions_validating: [{
+                inherit: 'instructions',
+                name: 'raceiat_instructions_validating',
+                templateUrl: 'raceiat_instructions_validating.jst', // Separate file or reused
+                title: 'IAT Instructions (validating)',
+                header: 'Second IAT'
+            }],
 
-        lastpage: [{
-            type: 'message',
-            name: 'lastpage',
-            templateUrl: 'lastpage.jst',
-            title: 'End',
-            //Uncomment the following if you want to end the study here.
-            //last:true, 
-            header: 'You have completed the study'
-        }], 
-        
-        //Use if you want to redirect the participants elsewhere at the end of the study
-        redirect:
-        [{ 
-			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
-        }],
-		
-		//This task waits until the data are sent to the server.
-        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
-    });
+            explicits: [{
+                type: 'quest',
+                name: 'explicits',
+                scriptUrl: 'explicits.js'
+            }],
 
-    API.addSequence([
-        { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
-        
-        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
+            raceiat: [{
+                type: 'time',
+                name: 'raceiat',
+                scriptUrl: 'raceiat.js'
+            }],
 
-        // apply touch only styles
-        {
-            mixer:'branch',
-            conditions: {compare:'global.$isTouch', to: true},
-            data: [
-                {
-                    type: 'injectStyle',
-                    css: [
-                        '* {color:red}',
-                        '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
-                        '[piq-page] > ol {margin: 15px;}',
-                        '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
-                        '.container {padding:5px;}',
-                        '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
-                        '[pi-quest]::after {clear: both;}',
-                        '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
-                        '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
+            lastpage: [{
+                type: 'message',
+                name: 'lastpage',
+                templateUrl: 'lastpage.jst',
+                title: 'End',
+                //Uncomment the following if you want to end the study here.
+                //last:true, 
+                header: 'You have completed the study'
+            }],
 
-                        '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
-                        '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
-                        '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
-                        // larger screens
-                        '@media (min-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
-                        '}',
-                        // phones and smaller screens
-                        '@media (max-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
-                        ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
-                        '}'
-                    ]
-                }
-            ]
-        },
-        
-        // First run (fresh round)
+            //Use if you want to redirect the participants elsewhere at the end of the study
+            redirect:
+                [{
+                    //Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
+                    type: 'redirect', name: 'redirecting', url: 'https://www.google.com/search'
+                }],
+
+            //This task waits until the data are sent to the server.
+            uploading: uploading_task({ header: 'just a moment', body: 'Please wait, sending data... ' })
+        });
+
+        API.addSequence([
+            { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
+
+            { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
+
+            // apply touch only styles
+            {
+                mixer: 'branch',
+                conditions: { compare: 'global.$isTouch', to: true },
+                data: [
+                    {
+                        type: 'injectStyle',
+                        css: [
+                            '* {color:red}',
+                            '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
+                            '[piq-page] > ol {margin: 15px;}',
+                            '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
+                            '.container {padding:5px;}',
+                            '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
+                            '[pi-quest]::after {clear: both;}',
+                            '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
+                            '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
+
+                            '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
+                            '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
+                            '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
+                            // larger screens
+                            '@media (min-width: 480px) {',
+                            ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
+                            '}',
+                            // phones and smaller screens
+                            '@media (max-width: 480px) {',
+                            ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
+                            ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
+                            '}'
+                        ]
+                    }
+                ]
+            },
+
+            // First run (fresh round)
             { inherit: 'intro' },
             {
                 mixer: 'random',
@@ -169,8 +187,8 @@ define(['managerAPI',
                 ]
             },
 
-// Second run (fresh round)
-            { inherit: 'intro' },
+            // Second run (fresh round)
+            { inherit: 'intro_2' },
             {
                 mixer: 'random',
                 data: [
@@ -185,10 +203,10 @@ define(['managerAPI',
                 ]
             },
 
-		{inherit: 'uploading'},
-        {inherit: 'lastpage'},
-        {inherit: 'redirect'}
-    ]);
+            { inherit: 'uploading' },
+            { inherit: 'lastpage' },
+            { inherit: 'redirect' }
+        ]);
 
-    return API.script;
-});
+        return API.script;
+    });
