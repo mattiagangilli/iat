5d4616f30de9c93af5897c0133fc7371822c0884
mattiagangilli <mattia.gangilli@gmail.com>
2025-07-02 10:43:26 +0200

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -1,117 +1,81 @@
-define(['questAPI'], function(Quest){
+define(['questAPI'], function(Quest){ 
     let API = new Quest();
     let isTouch = API.getGlobal().$isTouch;
-	
+
     /**
-	* Page prototype
-	*/
+     * Page prototype
+     */
     API.addPagesSet('basicPage',{
-        noSubmit:false, //Change to true if you don't want to show the submit button.
-        header: 'Questionnaire',
+        noSubmit:false,
+        header: 'Questionario',
         decline: true,
-        declineText: isTouch ? 'Decline' : 'Decline to Answer', 
+        declineText: isTouch ? 'Rifiuta' : 'Preferisco non rispondere', 
         autoFocus:true, 
-        progressBar:  'Page <%= pagesMeta.number %> out of 3'
+        progressBar: 'Pagina <%= pagesMeta.number %> di 1'
     });
-	
+
     /**
-	* Question prototypes
-	*/
+     * Question prototypes
+     */
     API.addQuestionsSet('basicQ',{
         decline: 'true',
-        required : true, 		
+        required : true,
         errorMsg: {
             required: isTouch 
-                ? 'Please select an answer, or click \'Decline\'' 
-                : 'Please select an answer, or click \'Decline to Answer\''
+                ? 'Seleziona una risposta o clicca su "Rifiuta"' 
+                : 'Seleziona una risposta o clicca su "Preferisco non rispondere"'
         },
         autoSubmit:'true',
-        numericValues:'true',
-        help: '<%= pagesMeta.number < 3 %>',
-        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
+        numericValues:'true'
     });
 
-    API.addQuestionsSet('basicSelect',{
-        inherit :'basicQ',
+    API.addQuestionsSet('basicSelect', {
+        inherit: 'basicQ',
         type: 'selectOne'
     });
-	
-    API.addQuestionsSet('basicDropdown',{
-        inherit :'basicQ',
-        type : 'dropdown',
-        autoSubmit:false
-    });
-	
-    API.addQuestionsSet('therm',{
-        inherit: 'basicSelect',
-        answers: [
-            {text:'10 - Extremely warm', value:10},
-            {text:'9 - Very warm', value:9},
-            {text:'8 - Moderately warm', value:8},
-            {text:'7 - Somewhat warm', value:7},
-            {text:'6 - Slightly warm', value:6},
-            {text:'5 - Neither warm nor cold', value:5},
-            {text:'4 - Slightly cold', value:4},
-            {text:'3 - Somewhat cold', value:3},
-            {text:'2 - Moderately cold', value:2},
-            {text:'1 - Very cold', value:1},
-            {text:'0 - Extremely cold', value:0}
-        ]
+
+    API.addQuestionsSet('basicDropdown', {
+        inherit: 'basicQ',
+        type: 'dropdown',
+        autoSubmit: false
     });
 
-	
     /**
-	*Specific questions
-	*/	
-    API.addQuestionsSet('attributes7',{
-        inherit : 'basicSelect',
-        name: 'attributes7',
-        stem: 'Which statement best describes you?',
+     * Sesso e Età
+     */
+    API.addQuestionsSet('gender', {
+        inherit: 'basicSelect',
+        name: 'gender',
+        stem: 'Qual è il tuo sesso?',
         answers: [
-            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
-            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
-            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
-            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
-            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
-            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
-            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
+            {text: 'Maschio', value: 'male'},
+            {text: 'Femmina', value: 'female'},
+            {text: 'Altro', value: 'other'}
         ]
     });
-	
-    API.addQuestionsSet('thermBlack',{
-        inherit : 'therm',
-        name: 'Tblack_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
+
+    API.addQuestionsSet('age', {
+        inherit: 'basicDropdown',
+        name: 'age',
+        stem: 'Quanti anni hai?',
+        answers: (function(){
+            let ans = [];
+            for (let i = 10; i <= 100; i++) {
+                ans.push({text: i.toString(), value: i});
+            }
+            return ans;
+        })()
     });
 
-    API.addQuestionsSet('thermWhite',{
-        inherit : 'therm',
-        name: 'Twhite_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
-    });
-
+    /**
+     * Sequenza del questionario
+     */
     API.addSequence([
         {
-            mixer : 'random', 
-            data : [
-                {
-                    mixer : 'random', 
-                    wrapper:true, 
-                    data : [
-                        {
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermBlack'}
-                        },
-                        {
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermWhite'}							
-                        }
-                    ]
-                },
-                {
-                    inherit:'basicPage', 
-                    questions: {inherit:'attributes7'}
-                }
+            inherit: 'basicPage',
+            questions: [
+                {inherit: 'gender'},
+                {inherit: 'age'}
             ]
         }
     ]);
